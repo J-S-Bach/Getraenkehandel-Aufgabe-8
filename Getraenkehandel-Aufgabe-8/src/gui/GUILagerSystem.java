beb0089aa91a177b4cab9493876da48ba0d8a0a2
Jan Bauer <jan.bauer@werkstoffbit.de>
2021-02-10 16:17:55 +0100

    #new GUI

--- Getraenkehandel-Aufgabe-8/src/gui/GUILagerSystem.java
+++ Getraenkehandel-Aufgabe-8/src/gui/GUILagerSystem.java
@@ -3,23 +3,32 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
-import javax.swing.JLayeredPane;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
-import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+
 
 public class GUILagerSystem {
 
 	private JFrame frame;
-	private JButton btnRebooking;
-	private JPanel panelStartpage;
-	private JButton btnSale;
-	private JPanel panelRebooking;
+	private JPanel panelMenue;
+	private JPanel panelBuchen;
+	private JPanel panelVerkauf;
+	private JTable tableBuchenVon;
+	private JTable tableBuchenNach;
+	private JTable tableVerkaufVon;
+	private JTextField txtBuchenMengeEingabe;
+	private JTextField txtVerkaufenMengeEingabe;
+
 
 	/**
 	 * Launch the application.
@@ -44,74 +53,184 @@ public class GUILagerSystem {
 		initialize();
 	}
 
+	public DefaultTableModel defaultModul(JTable table) {
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+		return model;
+	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// Frames---------
 		frame = new JFrame();
-		frame.setBounds(100, 100, 900, 650);
+		frame.setBounds(100, 100, 675, 489);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		// ---------------------------------------------------------------------------------------------
+		// Panels----------
+		final JPanel panelMenue = new JPanel();
+		frame.getContentPane().add(panelMenue, "name_473979631140500");
+		panelMenue.setLayout(null);
+		panelMenue.setVisible(true);
 
-		// Startpage--------------------------------------
-		JPanel panelStartpage = new JPanel();
-		frame.getContentPane().add(panelStartpage, "name_615469137820100");
-		panelStartpage.setLayout(null);
-		panelStartpage.setVisible(true);
+		final JPanel panelBuchen = new JPanel();
+		frame.getContentPane().add(panelBuchen, "name_473992955029000");
+		panelBuchen.setLayout(null);
+		panelBuchen.setVisible(false);
 
-		JPanel panelRebooking = new JPanel();
-		frame.getContentPane().add(panelRebooking, "name_615480645851900");
-		panelRebooking.setLayout(null);
-		panelRebooking.setVisible(false);
+		final JPanel panelVerkauf = new JPanel();
+		frame.getContentPane().add(panelVerkauf, "name_474000871403000");
+		panelVerkauf.setLayout(null);
+		panelVerkauf.setVisible(false);
 
-		JPanel panelSale = new JPanel();
-		frame.getContentPane().add(panelSale, "name_615482751312600");
-		panelSale.setLayout(null);
-		panelSale.setVisible(false);
-
-		// Rebooking--------------------------
-
-		JButton btnRebooking = new JButton("Rebooking");
-		btnRebooking.addActionListener(new ActionListener() {
+		// ---------------------------------------------------------------------------------------------
+		// PanelBuchen---------
+		JButton btnBuchen = new JButton("Buchen");
+		btnBuchen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelStartpage.setVisible(false);
-				panelRebooking.setVisible(true);
+				panelBuchen.setVisible(true);
+				panelMenue.setVisible(false);
 			}
 		});
-		btnRebooking.setBounds(171, 219, 117, 81);
-		panelStartpage.add(btnRebooking);
+		btnBuchen.setBounds(74, 108, 120, 50);
+		panelMenue.add(btnBuchen);
 
-		JButton btnRebookingBackToStart = new JButton("Back to startpage");
-		btnRebookingBackToStart.addActionListener(new ActionListener() {
+		JButton btnZurueckVonBuchen = new JButton("Zurück");
+		btnZurueckVonBuchen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelRebooking.setVisible(false);
-				panelStartpage.setVisible(true);
+				panelBuchen.setVisible(false);
+				panelMenue.setVisible(true);
 			}
 		});
-		btnRebookingBackToStart.setBounds(10, 568, 136, 32);
-		panelRebooking.add(btnRebookingBackToStart);
+		btnZurueckVonBuchen.setBounds(560, 416, 89, 23);
+		panelBuchen.add(btnZurueckVonBuchen);
 
-		// Sale--------------------------------
+		JButton btnBuchenBuchen = new JButton("Jetzt Buchen");
+		btnBuchenBuchen.setBounds(265, 221, 130, 23);
+		panelBuchen.add(btnBuchenBuchen);
 
-		JButton btnSaleBackToStart = new JButton("Back to startpage");
-		btnSaleBackToStart.addActionListener(new ActionListener() {
+		JComboBox cbBuchenVon = new JComboBox();
+		cbBuchenVon.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelRebooking.setVisible(false);
-				panelStartpage.setVisible(true);
+				
 			}
 		});
-		btnSaleBackToStart.setBounds(10, 568, 136, 32);
-		panelSale.add(btnSaleBackToStart);
+		cbBuchenVon.setModel(new DefaultComboBoxModel(new String[] { "Standort1", "Zentrallager", "Standort2" }));
+		cbBuchenVon.setToolTipText("");
+		cbBuchenVon.setBounds(49, 35, 135, 22);
+		panelBuchen.add(cbBuchenVon);
 
-		JButton btnSale = new JButton("Sale");
-		btnSale.addActionListener(new ActionListener() {
+		JComboBox cbBuchenNach = new JComboBox();
+		cbBuchenNach.setModel(new DefaultComboBoxModel(new String[] { "Zentrallager", "Standort1", "Standort2" }));
+		cbBuchenNach.setToolTipText("");
+		cbBuchenNach.setBounds(487, 35, 135, 22);
+		panelBuchen.add(cbBuchenNach);
+
+		JComboBox cbBuchenArtickel = new JComboBox();
+		cbBuchenArtickel.setModel(new DefaultComboBoxModel(new String[] { "Mineralwasser, still",
+				"Mineralwasser, mit Kohlensäure", "Apfelsaft", "Orangensaft", "Limonade", "Bier" }));
+		cbBuchenArtickel.setBounds(265, 100, 127, 22);
+		panelBuchen.add(cbBuchenArtickel);
+
+		JLabel lblBuchenVon = new JLabel("von:");
+		lblBuchenVon.setBounds(23, 39, 46, 14);
+		panelBuchen.add(lblBuchenVon);
+
+		JLabel lblBuchenNach = new JLabel("nach:");
+		lblBuchenNach.setBounds(453, 39, 46, 14);
+		panelBuchen.add(lblBuchenNach);
+
+		JLabel lblBuchenUmbuchen = new JLabel("Umbuchen:");
+		lblBuchenUmbuchen.setBounds(287, 39, 89, 14);
+		panelBuchen.add(lblBuchenUmbuchen);
+
+		JLabel lblBuchenMenge = new JLabel("Menge in Flaschen:");
+		lblBuchenMenge.setBounds(265, 156, 115, 14);
+		panelBuchen.add(lblBuchenMenge);
+
+		tableBuchenVon = new JTable();
+		tableBuchenVon.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "New column", "New column", "New column", "New column", "New column" }));
+		tableBuchenVon.setBounds(10, 100, 230, 180);
+		panelBuchen.add(tableBuchenVon);
+
+		tableBuchenNach = new JTable();
+		tableBuchenNach.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "New column", "New column", "New column", "New column", "New column" }));
+		tableBuchenNach.setBounds(419, 100, 230, 180);
+		panelBuchen.add(tableBuchenNach);
+
+		txtBuchenMengeEingabe = new JTextField();
+		txtBuchenMengeEingabe.setToolTipText("Eingabe der Menge");
+		txtBuchenMengeEingabe.setBounds(265, 170, 86, 20);
+		panelBuchen.add(txtBuchenMengeEingabe);
+		txtBuchenMengeEingabe.setColumns(10);
+
+		// ---------------------------------------------------------------------------------------------
+		// PanelVerkaufen
+		JButton btnVerkaufen = new JButton("Verkaufen");
+		btnVerkaufen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelStartpage.setVisible(false);
-				panelSale.setVisible(true);
+				panelVerkauf.setVisible(true);
+				panelMenue.setVisible(false);
 			}
 		});
-		btnSale.setBounds(546, 219, 117, 81);
-		panelStartpage.add(btnSale);
+		btnVerkaufen.setBounds(404, 108, 120, 50);
+		panelMenue.add(btnVerkaufen);
+
+		JButton btnZurueckVonVerkauf = new JButton("Zurück");
+		btnZurueckVonVerkauf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panelVerkauf.setVisible(false);
+				panelMenue.setVisible(true);
+			}
+		});
+		btnZurueckVonVerkauf.setBounds(560, 416, 89, 23);
+		panelVerkauf.add(btnZurueckVonVerkauf);
+
+		JButton btnVerkaufenVerkaufen = new JButton("Jetzt Verkaufen");
+		btnVerkaufenVerkaufen.setBounds(265, 221, 130, 23);
+		panelVerkauf.add(btnVerkaufenVerkaufen);
+
+		JComboBox cbVerkaufenVon = new JComboBox();
+		cbVerkaufenVon.setModel(new DefaultComboBoxModel(new String[] { "Zentrallager", "Standort1", "Standort2" }));
+		cbVerkaufenVon.setToolTipText("");
+		cbVerkaufenVon.setBounds(49, 35, 135, 22);
+		panelVerkauf.add(cbVerkaufenVon);
+
+		JComboBox cbVerkaufenArtickel = new JComboBox();
+		cbVerkaufenArtickel.setModel(new DefaultComboBoxModel(new String[] { "Mineralwasser, still",
+				"Mineralwasser, mit Kohlensäure", "Apfelsaft", "Orangensaft", "Limonade", "Bier" }));
+		cbVerkaufenArtickel.setBounds(265, 100, 127, 22);
+		panelVerkauf.add(cbVerkaufenArtickel);
+
+		JLabel lblVerkaufenVon = new JLabel("von:");
+		lblVerkaufenVon.setBounds(23, 39, 46, 14);
+		panelVerkauf.add(lblVerkaufenVon);
+
+		JLabel lblVerkaufenUmVerkaufen = new JLabel("UmVerkaufen:");
+		lblVerkaufenUmVerkaufen.setBounds(287, 39, 89, 14);
+		panelVerkauf.add(lblVerkaufenUmVerkaufen);
+
+		JLabel lblVerkaufMenge = new JLabel("Menge in Flaschen:");
+		lblVerkaufMenge.setBounds(265, 156, 115, 14);
+		panelVerkauf.add(lblVerkaufMenge);
+
+		tableVerkaufVon = new JTable();
+		tableVerkaufVon.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "New column", "New column", "New column", "New column", "New column" }));
+		tableVerkaufVon.setBounds(10, 100, 230, 180);
+		panelVerkauf.add(tableVerkaufVon);
+
+		txtVerkaufenMengeEingabe = new JTextField();
+		txtVerkaufenMengeEingabe.setToolTipText("Eingabe der Menge");
+		txtVerkaufenMengeEingabe.setBounds(265, 170, 86, 20);
+		panelVerkauf.add(txtVerkaufenMengeEingabe);
+		txtVerkaufenMengeEingabe.setColumns(10);
+
+		// ---------------------------------------------------------------------------------------------
 
 	}
 }
