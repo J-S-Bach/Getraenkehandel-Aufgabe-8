b267f170f3d872a630beb1284bbad099e75d489f
Fliens <fliens@web.de>
2021-02-10 16:33:57 +0100

    Added toString() for Location

--- Getraenkehandel-Aufgabe-8/src/main/LocationManager.java
+++ Getraenkehandel-Aufgabe-8/src/main/LocationManager.java
@@ -31,7 +31,7 @@ public class LocationManager {
 		standort1.setCapacity(new Beer(7), 150);
 		
 		central = zentral;
-		//System.out.println(central.toString());
+		System.out.println(central.toString());
 		locations[0] = standort1;
 		locations[1] = standort2;
 	}
