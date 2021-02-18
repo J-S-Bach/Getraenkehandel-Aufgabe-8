6e656b71cee5fc467b59d05035a6daa0628f4e8d
Fliens <fliens@web.de>
2021-02-18 16:17:51 +0100

    removed sys out

--- Getraenkehandel-Aufgabe-8/src/main/LocationManager.java
+++ Getraenkehandel-Aufgabe-8/src/main/LocationManager.java
@@ -31,7 +31,6 @@ public class LocationManager {
 		standort2.setCapacity(new Beer(7), 150);
 
 		central = zentral;
-		System.out.println(central.toString());
 		//System.out.println(central.toString());
 		locations[0] = standort1;
 		locations[1] = standort2;
