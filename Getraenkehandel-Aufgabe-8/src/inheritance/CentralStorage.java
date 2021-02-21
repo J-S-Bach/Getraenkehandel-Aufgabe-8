b2baf2c0a928b80e8ec1e14d4ce25356f03db052
Jan Bauer <jan.bauer@werkstoffbit.de>
2021-02-21 23:42:57 +0100

    #edited the max amount of bottles in central storage

--- Getraenkehandel-Aufgabe-8/src/inheritance/CentralStorage.java
+++ Getraenkehandel-Aufgabe-8/src/inheritance/CentralStorage.java
@@ -4,12 +4,12 @@ public class CentralStorage extends Location{
 
 	public CentralStorage(String name) {
 		super(name);
-		this.setCapacity(new WaterNonSparkling(BottleType.GLAS), 200);
-		this.setCapacity(new WaterSparkling(BottleType.PLASTIC), 400);
-		this.setCapacity(new AppleJuice(30), 200);
-		this.setCapacity(new OrangeJuice(40), 400);
+		this.setCapacity(new WaterNonSparkling(BottleType.GLAS), 198);
+		this.setCapacity(new WaterSparkling(BottleType.PLASTIC), 396);
+		this.setCapacity(new AppleJuice(30), 198);
+		this.setCapacity(new OrangeJuice(40), 396);
 		this.setCapacity(new Lemonade(35), 300);
-		this.setCapacity(new Beer(7), 200);
+		this.setCapacity(new Beer(7), 192);
 		this.fill();
 	}
 	
